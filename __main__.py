c19601f211a00346f3ec3794cac14d38a60b99c7
machi1206 <machiraju.shanmukh@gmail.com>
2025-10-18 10:58:05 +0530

    Define app entry point functionality

---
+++ __main__.py
@@ -0,0 +1,13 @@
+"""
+Purpose:
+Allows you to run the CV module independently of MIMS — for testing, debugging, or demos.
+
+What it does:
+Sets up logging and config.
+Instantiates CV_Module().
+Launches frontend.cv_window for UI visualization.
+Optionally runs a simple loop to show live detections.
+
+Used by:
+Devs
+"""
