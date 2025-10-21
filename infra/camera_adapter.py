050811361258ab74ebf1343640c5a0ae1b09e231
machi1206 <machiraju.shanmukh@gmail.com>
2025-10-21 05:48:35 +0530

    Create camera adapter class and boilerplate functions

--- infra/camera_adapter.py
+++ infra/camera_adapter.py
@@ -10,3 +10,39 @@ Handles resizing, error catching, and thread safety if needed.
 Used by:
 All app use-cases needing live images (validation, label scanning, face verification).
 """
+
+import numpy as np
+import cv2 as cv
+
+class CameraAdapter:
+    def __init__(self, src=0):
+        self.cam = None
+        self.src = src
+
+    def start_video(self):
+        self.cam = cv.VideoCapture(self.src)
+        if not self.cam.isOpened():
+            raise RuntimeError(f"Cannot open camera {self.src}")
+
+    def get_frame(self):
+        ret, frame = self.cam.read()
+        if not ret:
+            raise RuntimeError(f"Cannot receive frame from camera, exiting...")
+
+        return frame
+
+    def release(self):
+        if self.cam:
+            self.cam.release()
+
+
+
+# if __name__ == "__main__":
+#     cam = CameraAdapter(0)
+#     cam.start_video()
+#     while True:
+#         frame = cam.get_frame()
+#         cv.imshow("Camera", frame)
+#         if cv.waitKey(1) == ord('q'):
+#             break
+#     cam.release()
