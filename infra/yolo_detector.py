27df1ebcc02b7ed6de28b02c153ad47886ff83f1
machi1206 <machiraju.shanmukh@gmail.com>
2025-10-28 19:16:21 +0530

    Fixed default path and made necessary variables for communicating with Detection module.

--- infra/yolo_detector.py
+++ infra/yolo_detector.py
@@ -14,31 +14,43 @@ validate_transaction.py (and possibly scan_labels.py to locate label regions).
 from ..domain.models import Detection
 from ultralytics import YOLO
 from class_map import CLASS_MAP
-import time
 from pathlib import Path
 from typing import Optional
+import time
 
 class YOLODetector:
-    def __init__(self, weights_path: str = "./../models/yolov8.pt"):
-        self.weighs_path = Path(weights_path)
+    def __init__(self, weights_path: str = Path(__file__).resolve().parent.parent / "models" / "yolov8.pt"):
+        self.weights_path = Path(weights_path)
         if not self.weights_path.exists():
             raise FileNotFoundError(f"YOLO weights not found at {self.weights_path}")
         
-        self.yolo_model = YOLO(str(self.weighs_path))
+        self.yolo_model = YOLO(str(self.weights_path))
 
     def detect(self, frame, frame_id: Optional[int] = None):
         results = self.yolo_model(frame)
         detections = []
 
-        ## time in milli-seconds
+        ## time in milliseconds
         timestamp = int(time.time() * 1000)
 
         for result in results:
-            for box, class_id, confidence, in zip(result.boxes.xyxy, result.boxes.cls, result.boxes.conf):
-                detections.append(Detection(id=int(class_id), label=CLASS_MAP.get(int(class_id), f"Class-{class_id}"), confidence=float(confidence), bbox=tuple(box.tolist()), timestamp=timestamp, frame_id=frame_id))
+            for box in result.boxes:
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+                class_id = int(box.cls[0])
+                confidence = float(box.conf[0])
+                detections.append(
+                    Detection(
+                        id=class_id,
+                        label=CLASS_MAP.get(class_id, f"Class-{class_id}"),
+                        confidence=confidence,
+                        bbox=(x1, y1, x2, y2),
+                        timestamp=timestamp,
+                        frame_id=frame_id
+                    )
+                )
 
         return detections
-    
+
 
 # if __name__ == "__main__":
 #     import cv2
